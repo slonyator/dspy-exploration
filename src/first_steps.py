b817a53e87787abadf70ceac8477b5c53996ec0a
Michael Polonskij <michael.polonskij@gmx.de>
2025-02-21 21:18:37 +0100

    fix: Setting LM / first working example

--- src/first_steps.py
+++ src/first_steps.py
@@ -1,6 +1,9 @@
+import os
+
 from dotenv import load_dotenv
-from dspy import Signature, InputField, OutputField, Predict
+from dspy import Signature, InputField, OutputField, Predict, settings, LM
 from typing import Literal
+from loguru import logger
 
 
 class InformationExtractor(Signature):
@@ -17,9 +20,20 @@ class InformationExtractor(Signature):
 if __name__ == "__main__":
     _ = load_dotenv()
 
+    lm = LM(
+        model="openai/gpt-4o-mini",
+        api_key=os.environ["OPENAI_API_KEY"],
+        temperature=0,
+        max_tokens=1000,
+    )
+    settings.configure(lm=lm)
+
+    logger.info("Model settings finished")
     sample_text = "Der Versicherungsnehmer meldet einen Schaden am 01.01.2023."
 
     information = Predict(InformationExtractor)
     result = information(text=sample_text)
+
+    logger.info("Prediction finished")
 
     print(result)
