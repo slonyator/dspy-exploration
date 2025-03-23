987c9df171dd98f699273c565887e0f375024c2d
Michael Polonskij <michael.polonskij@gmx.de>
2025-03-23 19:07:07 +0100

    feat: initial dspy prediction

---
+++ test.py
@@ -0,0 +1,71 @@
+import os
+
+from dotenv import load_dotenv
+from pyprojroot import here
+from loguru import logger
+from sklearn.utils import shuffle
+from typing import Literal
+
+import dspy
+import pandas as pd
+
+
+class ClassifyText(dspy.Signature):
+    text: str = dspy.InputField(desc="Versicherungsschadenmeldung")
+
+    type: Literal[
+        "Teilkasko",
+        "Vollkasko",
+        "Glass",
+        "Einbruch / Diebstahl",
+        "Elementar",
+        "Feuer",
+        "Leitungswasser",
+        "Sturm",
+        "Allgemeine Haftpflicht",
+        "Haeuslicher Bereich",
+        "Handyschaden",
+        "Brillenschaden",
+        "Kraftfahrthaftpflicht",
+        "Sonstiges",
+    ] = dspy.OutputField(
+        desc="Wähle die Kategorie, die am besten zum Schaden basierend auf "
+        "dem bereitgestellten Dokument passt. Wähle 'Sonstiges' nur "
+        "dann, falls der Schaden keiner der anderen Kategorien "
+        "zugeordnet werden kann."
+    )
+
+    object: Literal[
+        "Hausrat",
+        "Wohngebäude",
+        "Kasko",
+        "Kraftfahrthaftpflicht",
+        "Allgemeine Haftpflicht",
+        "Glass",
+        "Sonstiges",
+    ] = dspy.OutputField(
+        desc="Wähle die Kategorie, die am besten zum beschädigten Objekt "
+        "basierend auf dem bereitgestellten Dokument passt. Wähle "
+        "'Sonstiges', falls keine der anderen Kategorien passt."
+    )
+if __name__ == "__main__":
+    logger.info("Loading data")
+    df = shuffle(pd.read_csv(here("./df_sample.csv")), random_state=42)
+
+    logger.info("Selecting testset for zero-shot learning")
+    test = df.tail(20)
+
+    logger.info("Loading OPENAI API KEY")
+
+    _ = load_dotenv()
+
+    logger.info("Setting dspy model")
+    lm = dspy.LM("openai/gpt-4o-mini", api_key=os.environ["OPENAI_API_KEY"])
+    dspy.configure(lm=lm)
+
+    logger.info("Zero-shot predictions")
+    zero_shot_predictor = dspy.Predict(ClassifyText)
+
+    predictions = [
+        zero_shot_predictor(text=text) for text in test["anonymized_text"]
+    ]
