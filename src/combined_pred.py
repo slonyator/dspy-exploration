978b8a5b7caec50d13a54178d93fbc0e8641e81c
Michael Polonskij <michael.polonskij@gmx.de>
2025-03-24 12:46:21 +0100

    feat: model validator for Output validation

--- src/combined_pred.py
+++ src/combined_pred.py
@@ -4,7 +4,55 @@ from typing import Literal
 from dotenv import load_dotenv
 from dspy import Signature, InputField, OutputField, Predict, LM, configure
 from loguru import logger
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
+from typing_extensions import Self
+
+
+class Mapper:
+    typ_mapping = {
+        "TK": "Teilkasko",
+        "VK": "Vollkasko",
+        "GL": "Glass",
+        "ED": "Einbruch / Diebstahl",
+        "EL": "Elementar",
+        "FE": "Feuer",
+        "LW": "Leitungswasser",
+        "ST": "Sturm",
+        "AH": "Allgemeine Haftpflicht",
+        "HB": "Haeuslicher Bereich",
+        "HY": "Handyschaden",
+        "BR": "Brillenschaden",
+        "KH": "Kraftfahrthaftpflicht",
+        "Other": "Sonstiges",
+    }
+
+    object_mapping = {
+        "HR": "Hausrat",
+        "WG": "Wohngebäude",
+        "KF": "Kasko",
+        "KH": "Kraftfahrthaftpflicht",
+        "AH": "Allgemeine Haftpflicht",
+        "GL": "Glass",
+        "Other": "Sonstiges",
+    }
+
+    @classmethod
+    def get_typ_full_name(cls, abbreviation):
+        return cls.typ_mapping.get(abbreviation, "Unknown")
+
+    @classmethod
+    def get_typ_abbreviation(cls, full_name):
+        reverse_typ_mapping = {v: k for k, v in cls.typ_mapping.items()}
+        return reverse_typ_mapping.get(full_name, "Unknown")
+
+    @classmethod
+    def get_object_full_name(cls, abbreviation):
+        return cls.object_mapping.get(abbreviation, "Unknown")
+
+    @classmethod
+    def get_object_abbreviation(cls, full_name):
+        reverse_object_mapping = {v: k for k, v in cls.object_mapping.items()}
+        return reverse_object_mapping.get(full_name, "Unknown")
 
 
 class BasisSchaden(BaseModel):
@@ -44,6 +92,41 @@ class BasisSchaden(BaseModel):
         "basierend auf dem bereitgestellten Dokument passt. Wähle "
         "'Sonstiges', falls keine der anderen Kategorien passt.",
     )
+
+    @model_validator(mode="after")
+    def check_valid_combination(self) -> Self:
+        valid_combinations = {
+            ("HR", "LW"),
+            ("HR", "ST"),
+            ("HR", "FE"),
+            ("HR", "EL"),
+            ("HR", "ED"),
+            ("HR", "GL"),
+            ("WG", "LW"),
+            ("WG", "ST"),
+            ("WG", "FE"),
+            ("WG", "EL"),
+            ("WG", "ED"),
+            ("WG", "GL"),
+            ("GL", "GL"),
+            ("KF", "VK"),
+            ("KF", "TK"),
+            ("KH", "KH"),
+            ("AH", "AH"),
+            ("AH", "HB"),
+            ("AH", "HY"),
+            ("AH", "BR"),
+        }
+
+        logger.info("Mapping schaden-objekt from full name to abbreviation")
+        self.objekt = Mapper.get_object_abbreviation(self.objekt)
+
+        logger.info("Mapping schaden-typ from full name to abbreviation")
+        self.typ = Mapper.get_typ_abbreviation(self.typ)
+
+        if (self.objekt, self.typ) not in valid_combinations:
+            raise ValueError("Kombination von Typ und Objekt ist nicht gültig")
+        return Self
 
 
 class FNOL(Signature):
